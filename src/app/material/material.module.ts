675e2488697403b70a36dbd95725bf4efb0871cf
Dmitry Gapeev <gapeev.dm@gmail.com>
2022-05-01 18:54:01 +0300

    feat: adjust material module

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,8 +1,23 @@
 import { NgModule } from '@angular/core';
+import { ErrorStateMatcher, ShowOnDirtyErrorStateMatcher } from '@angular/material/core';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+
+const MaterialComponents = [
+  MatFormFieldModule,
+  MatInputModule,
+  MatProgressBarModule,
+  MatButtonModule,
+  MatSnackBarModule,
+];
 
 @NgModule({
   declarations: [],
-  imports: [],
-  exports: [],
+  imports: [MaterialComponents],
+  exports: [MaterialComponents],
+  providers: [{ provide: ErrorStateMatcher, useClass: ShowOnDirtyErrorStateMatcher }],
 })
 export class MaterialModule {}
