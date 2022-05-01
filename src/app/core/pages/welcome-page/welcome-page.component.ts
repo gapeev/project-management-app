e23d96a9ccee101d546af883af0902e87e8b79b9
Dmitry Gapeev <gapeev.dm@gmail.com>
2022-05-01 19:16:08 +0300

    feat: add welcome page

---
+++ src/app/core/pages/welcome-page/welcome-page.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import { deleteUserPending, signOut } from '@store/actions/user.actions';
+import { selectIsUserAuth } from '@store/selectors/user.selectors';
+
+@Component({
+  selector: 'app-welcome-page',
+  templateUrl: './welcome-page.component.html',
+  styleUrls: ['./welcome-page.component.scss'],
+})
+export class WelcomePageComponent {
+  public get isUserAuth(): Observable<boolean> {
+    return this.store.select(selectIsUserAuth);
+  }
+
+  public signOut(): void {
+    this.store.dispatch(signOut());
+    this.router.navigateByUrl('/auth/sign-in');
+  }
+
+  public deleteUser(): void {
+    this.store.dispatch(deleteUserPending());
+  }
+
+  constructor(private store: Store, private router: Router) {}
+}
