75c1854aea1420a7fb1c1d08e5a43495ce4dc2c2
Dmitry Gapeev <gapeev.dm@gmail.com>
2022-05-01 19:39:01 +0300

    refactor: move ngrx to core module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,30 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { StoreModule } from '@ngrx/store';
-import { StoreDevtoolsModule } from '@ngrx/store-devtools';
-import { StoreRouterConnectingModule } from '@ngrx/router-store';
-import { EffectsModule } from '@ngrx/effects';
 import { AppRoutingModule } from './app-routing.module';
 import { CoreModule } from '@core/core.module';
 
 import { AppComponent } from './app.component';
-import reducers from '@store/reducers';
-import { environment } from '../environments/environment';
-import UserEffects from '@store/effects/user.effects';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule,
-    StoreModule.forRoot(reducers),
-    StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: environment.production }),
-    EffectsModule.forRoot([UserEffects]),
-    StoreRouterConnectingModule.forRoot(),
-    CoreModule,
-  ],
+  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule, CoreModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,13 +1,29 @@
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { CommonModule } from '@angular/common';
+import { SharedModule } from '@shared/shared.module';
+import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { StoreRouterConnectingModule } from '@ngrx/router-store';
+import { EffectsModule } from '@ngrx/effects';
 import { WelcomePageComponent } from './pages/welcome-page/welcome-page.component';
 import HttpInterceptors from './interceptors';
-import { SharedModule } from '@shared/shared.module';
+
+import reducers from '@store/reducers';
+import { environment } from '../../environments/environment';
+import UserEffects from '@store/effects/user.effects';
 
 @NgModule({
   declarations: [WelcomePageComponent],
-  imports: [CommonModule, SharedModule, HttpClientModule],
+  imports: [
+    CommonModule,
+    SharedModule,
+    HttpClientModule,
+    StoreModule.forRoot(reducers),
+    StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: environment.production }),
+    EffectsModule.forRoot([UserEffects]),
+    StoreRouterConnectingModule.forRoot(),
+  ],
   providers: [HttpInterceptors],
 })
 export class CoreModule {}
