b8f470910784e0650d9d5e06a0df61a5afef11dc
Dmitry Gapeev <gapeev.dm@gmail.com>
2022-05-01 16:32:40 +0300

    feat: adjust main reducer (use user and error reducers)

--- src/app/store/reducers/index.ts
+++ src/app/store/reducers/index.ts
@@ -1,6 +1,11 @@
 import { ActionReducerMap } from '@ngrx/store';
 import { State } from '@store/state.model';
+import { errorReducer } from './error.reducer';
+import { userReducer } from './user.reducer';
 
-const reducers: ActionReducerMap<State> = {};
+const reducers: ActionReducerMap<State> = {
+  user: userReducer,
+  error: errorReducer,
+};
 
 export default reducers;
