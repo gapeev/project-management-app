a5ef61db4c8475f3b8b044e5e7f240b510a39142
Dmitry Gapeev <gapeev.dm@gmail.com>
2022-05-01 19:17:04 +0300

    feat: adjust core module

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 import { CommonModule } from '@angular/common';
-import { MaterialModule } from '../material/material.module';
+import { WelcomePageComponent } from './pages/welcome-page/welcome-page.component';
+import HttpInterceptors from './interceptors';
+import { SharedModule } from '@shared/shared.module';
 
 @NgModule({
-  declarations: [],
-  imports: [CommonModule, MaterialModule],
-  exports: [MaterialModule],
+  declarations: [WelcomePageComponent],
+  imports: [CommonModule, SharedModule, HttpClientModule],
+  providers: [HttpInterceptors],
 })
 export class CoreModule {}
