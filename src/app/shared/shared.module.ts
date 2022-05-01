648d9a3f028edfb90e86c4a5d6f054c45f7b228f
Dmitry Gapeev <gapeev.dm@gmail.com>
2022-05-01 19:12:09 +0300

    feat: adjust shared module

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NotificationComponent } from './components/notification/notification.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MaterialModule } from '../material/material.module';
 
 @NgModule({
-  declarations: [],
-  imports: [CommonModule],
+  declarations: [NotificationComponent],
+  imports: [CommonModule, ReactiveFormsModule, MaterialModule],
+  exports: [ReactiveFormsModule, MaterialModule, NotificationComponent],
 })
 export class SharedModule {}
