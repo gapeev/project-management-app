9fc83d2df57e8655166a3dce102bcb286a58bede
valeria310 <valeriua.m@yandex.ru>
2022-05-02 18:33:57 +0300

    feat: add header and footer components

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,9 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MaterialModule } from '../material/material.module';
+import { FooterComponent } from './components/footer/footer.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    FooterComponent,
+    HeaderComponent
+  ],
   imports: [CommonModule, MaterialModule],
   exports: [MaterialModule],
 })
