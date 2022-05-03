46d9f18799aa22cb8524abcdd212b612bda704ed
Анастасия <Angelice92@mail.ru>
2022-05-03 23:47:35 +0300

    add html to board-main-component

--- src/app/board/board.module.ts
+++ src/app/board/board.module.ts
@@ -1,18 +1,17 @@
-import { NgModule } from '@angular/core';
+import {CUSTOM_ELEMENTS_SCHEMA, NgModule, NO_ERRORS_SCHEMA} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BoardRoutingModule } from './board-routing.module';
 import { BoardMainComponent } from './components/board-main/board-main.component';
 import { TaskComponent } from './components/task/task.component';
+import { MatButtonModule } from "@angular/material/button";
+import { MatCardModule } from "@angular/material/card";
 
 @NgModule({
-  declarations: [
-    BoardMainComponent,
-    TaskComponent
-  ],
-  imports: [CommonModule, BoardRoutingModule],
+  declarations: [BoardMainComponent, TaskComponent],
+  imports: [CommonModule, BoardRoutingModule, MatButtonModule, MatCardModule],
   providers: [],
-  schemas: [],
-  bootstrap: []
+  schemas: [NO_ERRORS_SCHEMA, CUSTOM_ELEMENTS_SCHEMA],
+  bootstrap: [BoardMainComponent]
 })
 
 export class BoardModule {}
