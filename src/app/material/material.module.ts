0855c6902c7b9c1e5ca4072d4cb55357a1fb21d9
valeria310 <valeriua.m@yandex.ru>
2022-05-03 15:11:05 +0300

    feat: add footer

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,8 +1,25 @@
 import { NgModule } from '@angular/core';
+import { ErrorStateMatcher, ShowOnDirtyErrorStateMatcher } from '@angular/material/core';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import {MatToolbarModule} from '@angular/material/toolbar';
+
+const MaterialComponents = [
+  MatFormFieldModule,
+  MatInputModule,
+  MatProgressBarModule,
+  MatButtonModule,
+  MatSnackBarModule,
+  MatToolbarModule
+];
 
 @NgModule({
   declarations: [],
-  imports: [],
-  exports: [],
+  imports: [MaterialComponents],
+  exports: [MaterialComponents],
+  providers: [{ provide: ErrorStateMatcher, useClass: ShowOnDirtyErrorStateMatcher }],
 })
 export class MaterialModule {}
