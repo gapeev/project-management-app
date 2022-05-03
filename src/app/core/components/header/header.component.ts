a06e097fbbbccb3649e16a44604b808863075d2e
valeria310 <valeriua.m@yandex.ru>
2022-05-03 22:09:14 +0300

    feat: make header sticky

--- src/app/core/components/header/header.component.ts
+++ src/app/core/components/header/header.component.ts
@@ -7,10 +7,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
   isLangToggle = false;
+  isPageScrolled = false;
+  headerClassList: string[] = ['header'];
 
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    window.addEventListener('scroll', () => {
+      if (window.pageYOffset > 0) {
+        if (!this.headerClassList.includes('scrolled')) this.headerClassList.push('scrolled');
+      } else {
+        if (this.headerClassList.includes('scrolled')) this.headerClassList.pop();
+      }
+      console.log(this.headerClassList);
+    });
+  }
 
   getLang() {
     return this.isLangToggle ? 'RU' : 'EN';
