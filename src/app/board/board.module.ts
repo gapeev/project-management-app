06595aeaab0ce35de6feeac8c4588b9687c66e0d
Dmitry Gapeev <gapeev.dm@gmail.com>
2022-05-09 14:32:46 +0300

    feat: adjust board module (use new components)

--- src/app/board/board.module.ts
+++ src/app/board/board.module.ts
@@ -2,9 +2,25 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { BoardRoutingModule } from './board-routing.module';
+import { BoardsPageComponent } from './pages/boards-page/boards-page.component';
+import { BoardPageComponent } from './pages/board-page/board-page.component';
+import { SharedModule } from '@shared/shared.module';
+import { CreateBoardComponent } from './components/create-board/create-board.component';
+import { CreateColumnComponent } from './components/create-column/create-column.component';
+import { CreateTaskComponent } from './components/create-task/create-task.component';
+import { UpdateTaskComponent } from './components/update-task/update-task.component';
+import { SearchTasksPageComponent } from './pages/search-tasks-page/search-tasks-page.component';
 
 @NgModule({
-  declarations: [],
-  imports: [CommonModule, BoardRoutingModule],
+  declarations: [
+    BoardsPageComponent,
+    BoardPageComponent,
+    CreateBoardComponent,
+    CreateColumnComponent,
+    CreateTaskComponent,
+    UpdateTaskComponent,
+    SearchTasksPageComponent,
+  ],
+  imports: [CommonModule, BoardRoutingModule, SharedModule],
 })
 export class BoardModule {}
