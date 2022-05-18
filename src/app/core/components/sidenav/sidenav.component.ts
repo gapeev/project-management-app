0e053952f39e83c68bd85e70a22cf25dc5683588
valeria310 <valeriua.m@yandex.ru>
2022-05-18 23:50:09 +0300

    feat: add localization

--- src/app/core/components/sidenav/sidenav.component.ts
+++ src/app/core/components/sidenav/sidenav.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input } from '@angular/core';
 import { MatSidenav } from '@angular/material/sidenav';
 import { Store } from '@ngrx/store';
+import { TranslateService } from '@ngx-translate/core';
 import { selectIsUserAuth } from '@store/selectors/user.selectors';
 import { Observable } from 'rxjs';
 
@@ -14,20 +15,37 @@ export class SidenavComponent {
   public sidenav!: MatSidenav;
 
   public authLinks = [
-    { href: '/welcome', icon: 'home_app_logo', text: 'Welcome', hasDivider: true },
-    { href: '/boards', icon: 'grid_view', text: 'Boards', hasDivider: true },
-    { href: '/user/update', icon: 'person', text: 'Edit profile', hasDivider: false },
+    { href: '/welcome', icon: 'home_app_logo', text: '', hasDivider: true },
+    { href: '/boards', icon: 'grid_view', text: '', hasDivider: true },
+    { href: '/user/update', icon: 'person', text: '', hasDivider: false },
   ];
 
   public nonAuthLinks = [
-    { href: '/welcome', icon: 'home_app_logo', text: 'Welcome', hasDivider: true },
-    { href: '/auth/sign-in', icon: 'login', text: 'Log In', hasDivider: false },
-    { href: '/auth/sign-up', icon: 'how_to_reg', text: 'Sign Up', hasDivider: false },
+    { href: '/welcome', icon: 'home_app_logo', text: '', hasDivider: true },
+    { href: '/auth/sign-in', icon: 'login', text: '', hasDivider: false },
+    { href: '/auth/sign-up', icon: 'how_to_reg', text: '', hasDivider: false },
   ];
 
   public get isAuth(): Observable<boolean> {
     return this.store.select(selectIsUserAuth);
   }
 
-  constructor(private store: Store) {}
+  constructor(private store: Store, private translate: TranslateService) {
+    this.translate.stream('HEADER.BUTTONS.WELCOME').subscribe((res) => {
+      this.authLinks[0].text = res;
+      this.nonAuthLinks[0].text = res;
+    });
+    this.translate.stream('HEADER.BUTTONS.BOARDS').subscribe((res) => {
+      this.authLinks[1].text = res;
+    });
+    this.translate.stream('HEADER.BUTTONS.EDIT').subscribe((res) => {
+      this.authLinks[2].text = res;
+    });
+    this.translate.stream('HEADER.BUTTONS.LOG-IN').subscribe((res) => {
+      this.nonAuthLinks[1].text = res;
+    });
+    this.translate.stream('HEADER.BUTTONS.SIGN-UP').subscribe((res) => {
+      this.nonAuthLinks[2].text = res;
+    });
+  }
 }
